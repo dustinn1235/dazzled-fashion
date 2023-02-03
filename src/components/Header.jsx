2aba4157081258408a1bbe426c89286c90a277db
dustinn <binhng1908@gmail.com>
2023-02-03 15:14:26 -0700

    fix header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,22 @@ const Header = () => {
           className="w-full h-full object-cover md:w-1/2 pointer-events-none"
         ></img>
       </Link>
-      <Link
-        to="/cart"
-        className="flex-1 text-end font-bold text-lg grid items-center"
-      >
-        CART ({numItem})
-      </Link>
+      <div className="flex-1 font-bold text-lg grid items-center">
+        <Link to="/cart" className="w-fit justify-self-end">
+          CART ({numItem})
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Header;
+
+/* <div className="flex-1 flex items-center justify-end gap-2">
+  <Link to="/cart" className="relative mr-4">
+    <AiOutlineShoppingCart className="text-5xl" />
+    <span className="absolute bg-black rounded-[50%] w-1/2 aspect-square text-white text-center top-0 right-[-25%]">
+      {numItem}
+    </span>
+  </Link>
+</div>; */
