33c68caf15e19b4dce864ea51c34421207f016e6
Jon Cho <jonathan.cho@hotmail.com>
2023-02-25 17:58:23 -0700

    validate data, update ordersRoute

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -2,11 +2,42 @@ const express = require("express");
 const router = express.Router();
 const db = require("../connect");
 
+const validateData = (req, res, next) => {
+  const { value } = req.body;
+  if (!value ||
+    !value.imgURL ||
+    !Array.isArray(value.imgURL) ||
+    value.imgURL.length !== 3 ||
+
+    !value.name ||
+    typeof value.name !== "string" ||
+
+    !value.price ||
+    typeof value.price !== "number" ||
+    value.price > 0 ||
+
+    !value.qty ||
+    typeof value.qty !== "number" ||
+    value.qty > 0 ||
+
+    !value.size ||
+    typeof value.size !== "string" ||
+    value.size.length !== 1 ||
+
+    !value.sizes ||
+    !Array.isArray(value.sizes) ||
+    !value.sizes.every((s) => typeof s === "string")
+  ) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+  next();
+};
+
 // get all items
-router.post("/", async (req, res) => {
+router.post("/",validateData, async (req, res) => {
   // TODO add data validation
   // TODO create order table and add the following order to the database
-  if (!req) res.status(418).send({ message: "Invalid data!" });
+  // if (!req) res.status(418).send({ message: "Invalid data!" });
   console.log(req.body);
   res.send(req.body);
 });
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 // use for local test connection with frontend. Default does not let frontend fetch from local ip api
 const corsOptions = {
-  origin: "http://127.0.0.1:5173",
+  origin: "http://localhost:5173",
 };
 
 const app = express();
