e195e0d93a897881624a7cee2c4c3c73db93e0ab
Jon Cho <jonathan.cho@hotmail.com>
2023-02-25 18:34:05 -0700

    update orders

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -3,33 +3,44 @@ const router = express.Router();
 const db = require("../connect");
 
 const validateData = (req, res, next) => {
-  const { value } = req.body;
-  if (!value ||
-    !value.imgURL ||
-    !Array.isArray(value.imgURL) ||
-    value.imgURL.length !== 3 ||
+  let total = 0;
+  // Check userName, subTotal, and date data types.
+  if (!req.body ||
+    !req.body.userName ||
+    typeof req.body.userName !== "string" ||
 
-    !value.name ||
-    typeof value.name !== "string" ||
+    !req.body.subTotal ||
+    typeof req.body.subTotal !== "number" ||
+    req.body.subTotal < 0 ||
 
-    !value.price ||
-    typeof value.price !== "number" ||
-    value.price > 0 ||
+    !req.body.date || 
+    typeof req.body.date !== "string" ||
 
-    !value.qty ||
-    typeof value.qty !== "number" ||
-    value.qty > 0 ||
-
-    !value.size ||
-    typeof value.size !== "string" ||
-    value.size.length !== 1 ||
-
-    !value.sizes ||
-    !Array.isArray(value.sizes) ||
-    !value.sizes.every((s) => typeof s === "string")
+    !req.body.items
   ) {
     return res.status(400).json({ message: "Invalid data" });
   }
+  // Check items in the cart
+  for (let i = 0; i < req.body.items.length; i++) {
+    if (!req.body.items[i].name ||
+      !req.body.items[i].price ||
+      !req.body.items[i].qty ||
+      !req.body.items[i].size ||
+      typeof req.body.items[i].name !== "string" ||
+      typeof req.body.items[i].size !== "string" ||
+      typeof req.body.items[i].qty !== "number" ||
+      typeof req.body.items[i].price !== "number" ||
+      req.body.items[i].qty < 0 || 
+      req.body.items[i].price < 0 
+    ){
+      return res.status(400).json({ message: "Invalid data" });
+    }
+    let itemTotal = req.body.items[i].qty * req.body.items[i].price;
+    total += itemTotal;
+  }
+
+  // If total of items in the cart not equal to subtotal, invalid request.
+  if (total !== req.body.subTotal) return res.status(400).json({ message: "Invalid data" });
   next();
 };
 
