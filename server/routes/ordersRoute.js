ef1ffe0d1aae81545c965b47ae939c58b4e48d29
dustinn <binhng1908@gmail.com>
2023-02-25 23:47:58 -0700

    create validation function

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -2,27 +2,26 @@ const express = require("express");
 const router = express.Router();
 const db = require("../connect");
 
-const validateData = (req, res, next) => {
+const validateData = (req) => {
   let total = 0;
   // Check userName, subTotal, and date data types.
-  if (!req.body ||
+  if (
+    !req.body ||
     !req.body.userName ||
     typeof req.body.userName !== "string" ||
-
     !req.body.subTotal ||
     typeof req.body.subTotal !== "number" ||
     req.body.subTotal < 0 ||
-
-    !req.body.date || 
+    !req.body.date ||
     typeof req.body.date !== "string" ||
-
     !req.body.items
   ) {
-    return res.status(418).json({ message: "Invalid data" });
+    return false;
   }
   // Check items in the cart
   for (let i = 0; i < req.body.items.length; i++) {
-    if (!req.body.items[i].name ||
+    if (
+      !req.body.items[i].name ||
       !req.body.items[i].price ||
       !req.body.items[i].qty ||
       !req.body.items[i].size ||
@@ -30,25 +29,25 @@ const validateData = (req, res, next) => {
       typeof req.body.items[i].size !== "string" ||
       typeof req.body.items[i].qty !== "number" ||
       typeof req.body.items[i].price !== "number" ||
-      req.body.items[i].qty < 0 || 
-      req.body.items[i].price < 0 
-    ){
-      return res.status(418).json({ message: "Invalid data" });
+      req.body.items[i].qty < 0 ||
+      req.body.items[i].price < 0
+    ) {
+      return false;
     }
     let itemTotal = req.body.items[i].qty * req.body.items[i].price;
     total += itemTotal;
   }
 
   // If total of items in the cart not equal to subtotal, invalid request.
-  if (total !== req.body.subTotal) return res.status(418).json({ message: "Invalid data" });
-  next();
+  if (total !== req.body.subTotal) return false;
+  return true;
 };
 
 // get all items
-router.post("/",validateData, async (req, res) => {
+router.post("/", validateData, async (req, res) => {
   // TODO add data validation
+  if (!validateData(req)) res.status(418).send({ message: "Invalid data!" });
   // TODO create order table and add the following order to the database
-  // if (!req) res.status(418).send({ message: "Invalid data!" });
   console.log(req.body);
   res.send(req.body);
 });
