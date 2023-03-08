f83f20bfde946883b3a15027de5a6ad8a4a7b13c
paul.latkovic <pauljlatkovic@gmail.com>
2023-03-07 21:08:45 -0700

    connection succesful to db using sqlite

--- server/connect.js
+++ server/connect.js
@@ -1,17 +1,31 @@
 // this file is used to set up connection to mysql server
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PWD,
-  database: process.env.MYSQL_DB,
-  port: process.env.MYSQL_PORT,
+const sqlite3 = require("sqlite3").verbose();
+
+/* 
+  - Can open the database in memory or on disk. We can create an in memory version  for read operations (forgot exactly what the TA said)
+    - Disk version to be stored and updated with the server
+
+  - Can open the database in 3 ways
+    - Read only (sqlite3.OPEN_READONLY)
+    - Read and write (sqlite3.OPEN_READWRITE)
+    - Read and write, create if it doesn't exist (sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
+      - Opens the database. If it doesnt exist it will create a new one
+*/
+let db = new sqlite3.Database('./db/database.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to database!");
-});
+
+// q: what does db.serialize do?
+
+// db.serialize(() => {
+//   db.run(S
+//     'CREATE TABLE IF NOT EXISTS')
+// });
 
 module.exports = db;
