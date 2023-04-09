ae963b17e3bc9eea17cef87ad02c89f1ac427156
Jon Cho <jonathan.cho@hotmail.com>
2023-04-09 14:52:39 -0600

    Edit console log

--- server/routes/ordersRoute.js
+++ server/routes/ordersRoute.js
@@ -86,7 +86,7 @@ router.post("/", async (req, res) => {
 
             // Increment logical clock for every request
             let timestamp = incrementClock();
-            console.log("ts is " + timestamp);
+            console.log("Logical Time is: " + timestamp);
 
             // if the qty of the item is greater than or equal to the qty in the request body
             // timestamp is date of query, update is the query
